f01459b3478c45325defaec5bf703c460b6a932b
hyemi-bang <pbhm1124@gmail.com>
2024-05-20 18:19:38 +0900

    프로그래머스 / 직사각형 넓이 구하기 Arrays.stream 메서드 이용하여 최소값 최대값 추출 하여 계산.

---
+++ src/coding_quiz/Multiplication.java
@@ -0,0 +1,42 @@
+package coding_quiz;
+
+import java.util.Arrays;
+
+public class Multiplication {
+
+	public static int solution(int[][] dots) {
+
+		// 주어질 좌표에서 x,y 값 추출
+
+		int[] xCoords = new int[dots.length];
+		int[] yCoords = new int[dots.length];
+
+		// 직사각형 네 꼭짓점의 좌표 [[x1, y1], [x2, y2], [x3, y3], [x4, y4]]
+		for (int i = 0; i < dots.length; i++) {
+			xCoords[i] = dots[i][0];
+			yCoords[i] = dots[i][1];
+		}
+
+		// 최대값 최소값
+		int xMin = Arrays.stream(xCoords).min().getAsInt();
+		int xMax = Arrays.stream(xCoords).max().getAsInt();
+		int yMin = Arrays.stream(yCoords).min().getAsInt();
+		int yMax = Arrays.stream(yCoords).max().getAsInt();
+
+		int width = xMax - xMin;
+		int height = yMax - yMin;
+
+		return width * height;
+
+	}
+
+	public static void main(String[] args) {
+
+		int[][] dots1 = { { 1, 1 }, { 2, 1 }, { 2, 2 }, { 1, 2 } };
+		int[][] dots2 = { { -1, -1 }, { 1, 1 }, { 1, -1 }, { -1, 1 } };
+
+		System.out.println(solution(dots1)); // Output: 1
+		System.out.println(solution(dots2)); // Output: 4
+	}
+
+}
